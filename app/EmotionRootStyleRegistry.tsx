532df8b938a54b5e0ac3b03f1e41d9f52a4ef09c
Rob Caldecott <robert.caldecott@zego.com>
2022-11-30 10:28:16 +0000

    Type tweak

--- app/EmotionRootStyleRegistry.tsx
+++ app/EmotionRootStyleRegistry.tsx
@@ -2,7 +2,7 @@
 import { CacheProvider } from "@emotion/react";
 import createCache from "@emotion/cache";
 import { useServerInsertedHTML } from "next/navigation";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { purple, green } from "@mui/material/colors";
 
@@ -11,7 +11,7 @@ const theme = createTheme({ palette: { primary: purple, secondary: green } });
 export default function RootStyleRegistry({
   children,
 }: {
-  children: JSX.Element;
+  children: ReactNode;
 }) {
   const [{ cache, flush }] = useState(() => {
     const cache = createCache({ key: "my" });
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import { ReactNode } from "react";
 import EmotionRootStyleRegistry from "./EmotionRootStyleRegistry";
 
-export default function RootLayout({ children }: { children: JSX.Element }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
       <body>
